2efb45493270c95fd34ec84b9c4f2387d77063ba
Kyle James <ksjames49@gmail.com>
2024-07-28 21:32:04 -0600

    Moved "Create New" button to inside label dropdown. Changed progress bar color based on timer type.

--- PomodoroUI/PomodoroUI/Pages/Manage/Index.cshtml.cs
+++ PomodoroUI/PomodoroUI/Pages/Manage/Index.cshtml.cs
@@ -1,12 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PomodoroLibrary.Models.Tables.StudyTaskEntities;
+using PomodoroLibrary.Services.Interfaces;
 
-namespace PomodoroUI.Pages.Manage
+namespace PomodoroUI.Pages.Manage;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly IStudyTaskService _studyTaskService;
+    private readonly IUserService _userService;
+
+    public IndexModel(IStudyTaskService studyTaskService, IUserService userService)
     {
-        public void OnGet()
-        {
-        }
+        _studyTaskService = studyTaskService;
+        _userService = userService;
+    }
+
+    public ICollection<StudyTask> StudyTasks { get; set; }
+
+    public async Task<IActionResult> OnGetAsync()
+    {
+        var user = await _userService.GetCurrentUserAsync();
+
+        if (user == null) return Challenge();
+
+        StudyTasks = await _studyTaskService.GetAllAsync(user.Id);
+
+        return Page();
     }
 }
