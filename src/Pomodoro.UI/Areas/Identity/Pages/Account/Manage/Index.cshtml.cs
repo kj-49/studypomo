07ee766c7391013e0f06ee68928b8cb884678b26
Kyle James <ksjames49@gmail.com>
2024-08-21 11:19:59 -0600

    User settings UI templated.

---
+++ src/Pomodoro.UI/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pomodoro.UI.Areas.Identity.Pages.Account.Manage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
