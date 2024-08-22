81086bc41f32932e8d008d8514b54794775faca7
Kyle James <ksjames49@gmail.com>
2024-08-21 22:07:47 -0600

    Password change functional.

--- src/Pomodoro.UI/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/Pomodoro.UI/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -3,10 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore.Query.Internal;
+using Pomodoro.Library.Migrations;
 using Pomodoro.Library.Models.Identity;
+using Pomodoro.Library.Models.Utility;
 using Pomodoro.Library.Services;
 using Pomodoro.Library.Services.Interfaces;
 using Pomodoro.UI.Util.PageModels;
+using System.ComponentModel.DataAnnotations;
 using TimeZoneConverter;
 
 namespace Pomodoro.UI.Areas.Identity.Pages.Account.Manage;
@@ -14,10 +17,20 @@ namespace Pomodoro.UI.Areas.Identity.Pages.Account.Manage;
 public class IndexModel : BaseModel
 {
     private readonly IUserService _userService;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(IUserService userService, UserManager<ApplicationUser> userManager)
+    public IndexModel(
+        IUserService userService,
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager,
+        ILogger<IndexModel> logger)
     {
         _userService = userService;
+        _userManager = userManager;
+        _signInManager = signInManager;
+        _logger = logger;
     }
 
     public bool IsExternallyAuthenticated { get; set; }
@@ -25,6 +38,9 @@ public class IndexModel : BaseModel
 
     [BindProperty]
     public string? IanaTimeZone { get; set; }
+
+    [BindProperty]
+    public PasswordModel PasswordInput { get; set; }
 
 
     public async Task<IActionResult> OnGetAsync()
@@ -52,8 +68,51 @@ public class IndexModel : BaseModel
         return RedirectToPage();
     }
 
-    public async Task<IActionResult> OnPageChangePasswordAsync()
+    public async Task<IActionResult> OnPostChangePasswordAsync()
     {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+        {
+            return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+        }
+
+        var changePasswordResult = await _userManager.ChangePasswordAsync(user, PasswordInput.OldPassword, PasswordInput.NewPassword);
+        if (!changePasswordResult.Succeeded)
+        {
+            foreach (var error in changePasswordResult.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+            return Page();
+        }
+
+        await _signInManager.RefreshSignInAsync(user);
+
         return RedirectToPage();
     }
+
+    public class PasswordModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+
 }
