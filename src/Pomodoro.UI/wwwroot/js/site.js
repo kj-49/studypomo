296b1cd10062245e964147c550aa4e4c9cbe8338
Kyle James <94993984+kj-49@users.noreply.github.com>
2024-09-04 20:08:22 -0600

    Truncated long studytask names and added an ellipsis. (#104)

--- src/Pomodoro.UI/wwwroot/js/site.js
+++ src/Pomodoro.UI/wwwroot/js/site.js
@@ -64,3 +64,6 @@ document.addEventListener(
     },
     false,
 );
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
