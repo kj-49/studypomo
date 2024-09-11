65e3d21a2ed5e6c783f2acf88d6829cc8717d3c6
Kyle James <94993984+kj-49@users.noreply.github.com>
2024-09-11 13:23:46 -0600

    Deadlines are now color coded according to time remaining. (#122)

---
+++ src/Pomodoro.UI/Util/TagHelpers/DeadlineTagHelper.cs
@@ -0,0 +1,38 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Pomodoro.UI.Util.TagHelpers;
+
+public class DeadlineTagHelper : TagHelper
+{
+    public DateTime? Date { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.TagName = "span";
+
+        string textColor = string.Empty;
+
+        if (Date.HasValue)
+        {
+            TimeSpan timeToDeadline = Date.Value - DateTime.UtcNow;
+
+            if (timeToDeadline.TotalDays < 1)
+            {
+                textColor = "text-danger";
+            }
+            else
+            {
+                textColor = "text-muted";
+            }
+        }
+
+
+
+        output.Attributes.SetAttribute("class", textColor);
+
+        // Modify the text if priority is set
+        string displayText = Date.Humanize();
+        output.Content.SetContent(displayText);
+    }
+}
