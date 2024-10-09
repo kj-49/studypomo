00d3f678d33614496993dea0864598f19b60d6cf
Kyle James <94993984+kj-49@users.noreply.github.com>
2024-10-09 10:19:24 -0600

    Added slow query detection logging (#204)

---
+++ src/StudyPomo.Library/Data/Database/SlowQueryDetectionHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyPomo.Library.Data.Database;
+
+public class SlowQueryDetectionHelper : DbCommandInterceptor
+{
+
+    private const int slowQueryThresholdInMilliSecond = 3000;
+    public override ValueTask<DbDataReader> ReaderExecutedAsync(DbCommand command, CommandExecutedEventData eventData, DbDataReader result, CancellationToken cancellationToken = default)
+    {
+        if (eventData.Duration.TotalMilliseconds > slowQueryThresholdInMilliSecond)
+        {
+            Log.Warning($"Slow Query Detected. {command.CommandText}  TotalMilliSeconds: {eventData.Duration.TotalMilliseconds}");
+        }
+        return base.ReaderExecutedAsync(command, eventData, result, cancellationToken);
+    }
+    public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
+    {
+        if (eventData.Duration.TotalMilliseconds > slowQueryThresholdInMilliSecond)
+        {
+            Log.Warning($"Slow Query Detected. {command.CommandText}  TotalMilliSeconds: {eventData.Duration.TotalMilliseconds}");
+        }
+        return base.ReaderExecuted(command, eventData, result);
+    }
+}
