5e69f33c60713d66de6a1f40a3ff0fe4a46c6c38
Kyle James <ksjames49@gmail.com>
2024-10-10 10:42:54 -0600

    Onboarding views created and IsOnboarded property added

---
+++ src/StudyPomo.UI/Areas/Identity/Pages/Onboarding/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudyPomo.UI.Areas.Identity.Pages.Onboarding
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
