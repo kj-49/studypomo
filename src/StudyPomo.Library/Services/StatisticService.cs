2560129b23e665e7671b1d5909b3b3eabf81bd02
Kyle James <94993984+kj-49@users.noreply.github.com>
2024-10-13 14:15:49 -0600

    Fix streak calculation and add additional streak tests (#239)

--- src/StudyPomo.Library/Services/StatisticService.cs
+++ src/StudyPomo.Library/Services/StatisticService.cs
@@ -20,47 +20,18 @@ public class StatisticService : IStatisticService
 
     public int GetCurrentStreak(IEnumerable<StudySession> studySessions)
     {
-        if (studySessions.Count() == 0)
-        {
-            return 0;
-        }
-
-        if (!studySessions.All(u => u.UserId == studySessions.First().UserId))
-        {
-            throw new ArgumentException("Study sessions must be for the same user");
-        }
-
-
-        var daysWithPomodoros = studySessions
-            .Where(s => s.TotalPomodoros > 0)
-            .GroupBy(s => s.DateStarted.Date)
-            .Select(g => g.Key)
-            .OrderByDescending(date => date)
-            .ToList();
-
-        int streak = 0;
-        DateTime? previousDate = null;
-
-        foreach (DateTime date in daysWithPomodoros)
-        {
-            if (previousDate == null || date == previousDate.Value.AddDays(-1))
-            {
-                streak++;
-                previousDate = date;
-            }
-            else
-            {
-                break;
-            }
-        }
-
-        return streak;
+        return ComputeStreak(studySessions);
     }
 
     public async Task<int> GetCurrentStreakAsync(int userId)
     {
         IEnumerable<StudySession> studySessions = await _studySessionService.GetAllAsync(userId);
 
+        return ComputeStreak(studySessions);
+    }
+
+    private int ComputeStreak(IEnumerable<StudySession> studySessions)
+    {
         if (studySessions.Count() == 0)
         {
             return 0;
@@ -80,11 +51,25 @@ public class StatisticService : IStatisticService
             .ToList();
 
         int streak = 0;
-        DateTime? previousDate = null;
 
-        foreach (DateTime date in daysWithPomodoros)
+        if (daysWithPomodoros.Count == 0)
         {
-            if (previousDate == null || date == previousDate.Value.AddDays(-1))
+            return streak;
+        }
+
+        DateTime previousDate = daysWithPomodoros.First();
+        
+        // If today or yesterday, we have a running streak.
+        if (previousDate.Date == DateTime.Today || previousDate.Date == DateTime.Now.AddDays(-1).Date)
+        {
+            streak++;
+        }
+
+        IEnumerable<DateTime> remainingDates = daysWithPomodoros.Skip(1);
+
+        foreach (DateTime date in remainingDates)
+        {
+            if (previousDate.AddDays(-1).Date == date.Date)
             {
                 streak++;
                 previousDate = date;
