2abd1cf23101868e339fa561fa8bf31dd6e9e7d9
Kyle James <94993984+kj-49@users.noreply.github.com>
2024-10-17 19:45:41 -0600

    Add full datetime for due dates in a popover (#249)

--- src/StudyPomo.UI/Util/TagHelpers/DeadlineTagHelper.cs
+++ src/StudyPomo.UI/Util/TagHelpers/DeadlineTagHelper.cs
@@ -5,6 +5,9 @@ namespace StudyPomo.UI.Util.TagHelpers;
 
 public class DeadlineTagHelper : TagHelper
 {
+    [HtmlAttributeName("display-tz")]
+    public string DisplayTimeZone { get; set; }
+
     public DateTime? Date { get; set; }
 
     public override void Process(TagHelperContext context, TagHelperOutput output)
@@ -29,13 +32,26 @@ public class DeadlineTagHelper : TagHelper
             {
                 textColor = "text-muted";
             }
-
-
         }
 
-
-
         output.Attributes.SetAttribute("class", textColor);
+
+        if (Date.HasValue && DisplayTimeZone is not null)
+        {
+            var timezone = TimeZoneInfo.FindSystemTimeZoneById(DisplayTimeZone);
+
+            if (timezone is not null)
+            {
+                var localTime = TimeZoneInfo.ConvertTimeFromUtc(Date.Value, timezone);
+
+                output.Attributes.SetAttribute("data-bs-toggle", "popover");
+                output.Attributes.SetAttribute("data-bs-trigger", "hover focus");
+                output.Attributes.SetAttribute("data-bs-placement", "top");
+                output.Attributes.SetAttribute("data-bs-content", localTime.ToString());
+                output.Attributes.SetAttribute("data-bs-delay", "{ \"show\": 300, \"hide\": 100 }"); // Delayed popover
+            }
+
+        }
 
         // Modify the text if priority is set
         string displayText = Date.Humanize();
